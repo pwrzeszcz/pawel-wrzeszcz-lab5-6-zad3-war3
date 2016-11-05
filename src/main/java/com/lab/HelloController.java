eef2d22de8a327ffc0fe55f815a5183570f6d4e3
pavon147 <pawel20@10g.pl>
2016-11-05 13:19:49 +0100

    Initial commit

---
+++ src/main/java/com/lab/HelloController.java
@@ -0,0 +1,46 @@
+package com.lab;
+
+import org.springframework.social.connect.Connection;
+import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.facebook.api.Facebook;
+import org.springframework.social.facebook.api.PagedList;
+import org.springframework.social.facebook.api.Post;
+import org.springframework.social.facebook.api.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HelloController {
+
+    private Facebook facebook;
+    private ConnectionRepository connectionRepository;
+
+    public HelloController(Facebook facebook, ConnectionRepository connectionRepository)
+    {
+        this.facebook = facebook;
+        this.connectionRepository = connectionRepository;
+    }
+
+    @GetMapping
+    public String helloFacebook(Model model)
+    {
+        if (connectionRepository.findPrimaryConnection(Facebook.class) == null)
+        {
+            return "redirect:/connect/facebook";
+        }
+
+        String [] fields = { "id", "email",  "first_name", "last_name" };
+        User userProfile = facebook.fetchObject("me", User.class, fields);
+
+        model.addAttribute("facebookProfile", userProfile);
+        PagedList<User> friends = facebook.friendOperations().getFriendProfiles();
+        System.out.println("Ilość: " + friends.size());
+        model.addAttribute("friends", friends);
+
+        return "hello";
+    }
+
+}
